fe124a9e925e2851791b723143118d8389b3b76a
jaroslav <jazzoslav@gmail.com>
2013-11-10 17:43:18 +0100

    append to history file

--- paste.sprunge.sh
+++ paste.sprunge.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -F 'sprunge=<-' http://sprunge.us > ~/.sprunges.history 2>>~/.sprunges.errata
+curl -F 'sprunge=<-' http://sprunge.us >> ~/.sprunges.history 2>>~/.sprunges.errata
 echo ff "`tail -n1 ~/.sprunges.history`"
