d181888d5e6e05bf2d196d083eb72823a7827e47
jaroslav <jazzoslav@gmail.com>
2013-12-30 03:12:31 +0100

    New wmctrl-1st2last based on awk

--- wmctrl-1st2last.sh
+++ wmctrl-1st2last.sh
@@ -1,18 +1,14 @@
-#!/bin/bash
+#!/bin/dash
 
-if [ ! -x "`which wmctrl 2>/dev/null`" ];
-then
-	echo plx install wmctrl
-	exit 1;
-fi
+wmctrl -d | awk '
+{ d=$1 }
 
-if `wmctrl -d|grep -q '^0.*\*'`;
-then 
-	# gives 0 when on first
-	echo go to last
-	wmctrl -s `wmctrl -d|tail -n1|cut -c 1` ; 
-else 
-	# gives 1 when on last
-	echo go to first
-	wmctrl -s0;
-fi 
+$2 ~ /\*/ { D=$1 }
+
+END { 
+	if (D > 0) {
+		goto=0 } 
+	else {
+		goto=d }
+	system("wmctrl -s " goto) 
+}'
