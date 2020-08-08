a988f91f5c9f7c35efcba1f4e175e9e07142d2f4
Jaroslav Rakhmatoullin <jazzoslav@gmail.com>
2020-08-08 09:25:02 +0200

    updates august

--- wmctrl-1st2last.sh
+++ wmctrl-1st2last.sh
@@ -1,22 +1,35 @@
-#!/bin/dash
-
-
+#!/bin/bash
 LAST=$2
-
 if [ $# -lt 2 ]; 
 then
 	LAST=`wmctrl -d | sed -n '${ s/\([0-9]\+\).*/\1/ ;p }'`
 fi
 
-wmctrl -d | awk -v first=${1:-0} -v last=${LAST:-1} '
+function swap() {
+	wmctrl -d | awk -v first=${1:-0} -v last=${LAST:-1} '
+		$2 ~ /\*/ { active=$1 }
+		END { 
+			if (active != first) {	goto=first } 
+			else { 					goto=last }
+			system("wmctrl -s " goto) 
+		}'
+}
 
-#{ d=$1 }
+swap $1
 
-$2 ~ /\*/ { active=$1 }
+# screens=($(
+# 	for d  in {0..3}; do
+# 		for s in {0..3}; do
+# 			ds=:$d.$s
+# 			DISPLAY=$ds xrandr &>/dev/null && echo $ds
+# 		done
+# 	done
+# 	))
+# 
+# for i in ${screens[@]}; do
+# 	echo DISPLAY=$i swap $1
+# 	DISPLAY=$i swap $1
+# done
 
-END { 
-	if (active != first) {	goto=first } 
-	else { 					goto=last }
 
-	system("wmctrl -s " goto) 
-}'
+sleep 0.09 && wmctrl-notify.sh
--- wmctrl-rotate.sh
+++ wmctrl-rotate.sh
@@ -1,35 +1,20 @@
 #!/bin/bash
-
 # $ wmctrl -d 
-
 # 0  - DG: 1680x2250  VP: N/A  WA: 0,0 1680x2250  firefox
 # 1  * DG: 1680x2250  VP: 0,0  WA: 0,0 1680x2250  terms
 # 2  - DG: 1680x2250  VP: N/A  WA: 0,0 1680x2250  misc
 # 3  - DG: 1680x2250  VP: N/A  WA: 0,0 1680x2250  gui
-
 # $ wmctrl -d | sed -n '/[0-9]\+ \+\*/ { s/ \+.*// ;p}'
 # 1
 
 WMCTRLD=$(wmctrl -d)
-
-DESKTOPS=(`echo -e "$WMCTRLD" | sed -n '
-	/^[0-9]/ { 
-		s/ \+.*// ;
-		p
-	}'| tr $'\n' ' '`)
-
+DESKTOPS=(`echo -e "$WMCTRLD" | sed -n '/^[0-9]/ {  s/ \+.*// ; p; }' | tr $'\n' ' '`)
+CURRENT=$(echo -e "$WMCTRLD" | sed -n '/[0-9]\+ \+\*/ { s/ \+.*// ;p }')
 LASTINDEX=$(( ${#DESKTOPS[@]} -1  )) 
-CURRENT=$( echo -e "$WMCTRLD" | sed -n '
-	/[0-9]\+ \+\*/ { 
-		s/ \+.*// ;p
-	}')
+NEXT=$((CURRENT+1))
+PREV=$((CURRENT-1))
 
-NEXT=$(( CURRENT+1))
-PREV=$(( CURRENT-1))
-
-echo CURRENT $CURRENT
-
-# wrap to the last / first destop from the first / last
+# wrap to the last to first; from the first to last
 [ $PREV -eq -1 ] && PREV=$LASTINDEX
 [ $NEXT -gt $LASTINDEX ] && NEXT=0
 
@@ -37,3 +22,4 @@ echo CURRENT $CURRENT
 [ "$1" == "prev" ] && wmctrl -s $PREV
 [ "$1" == "" ] && echo -e "$0: rotates the desktop\nusage: $0 [next|prev] "
 
+sleep 0.09 && wmctrl-notify.sh
