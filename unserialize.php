75a1240876958be171964df8992fa65eec5ec27a
Jaroslav Rakhmatoullin <jazzoslav@gmail.com>
2022-04-08 07:14:03 +0200

    update...

--- unserialize.php
+++ unserialize.php
@@ -1,6 +1,8 @@
 #!/usr/bin/php
 <?php
 
+// todo: strcmp(a, b)
+
 $fn = basename($argv[0], '.php');
 
 if (! function_exists($fn)) {
@@ -9,12 +11,15 @@ if (! function_exists($fn)) {
     exit(1);
 }
 
-if (count($argv) == 2) {
-    print_r($fn(
-        is_file($argv[1])
-            ? file_get_contents($argv[1])
-            : $argv[1]
-    ));
+if (count($argv) == 3) {
+    echo $fn($argv[1], $argv[2]);
+
+} elseif (count($argv) == 2) {
+    echo $fn(
+           is_file($argv[1])
+              ? file_get_contents($argv[1])
+              : $argv[1]
+         );
 } else {
     //$h1 = fopen('r', STDIN);
 
